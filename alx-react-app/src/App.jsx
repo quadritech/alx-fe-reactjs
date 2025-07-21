698f2dfd7b198ab4ae44eddbbc942ede3705b570
quadritech <96469908+quadritech@users.noreply.github.com>
2025-07-21 15:42:34 +0100

    creating more react components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,7 @@ import WelcomeMessage from './components/WelcomeMessag';
 import Header from './Header';
 import MainContent from './MainContent';
 import Footer from './Footer';
+import UserProfile from "./components/UserProfile";
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'

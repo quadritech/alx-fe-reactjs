312714208a1bcbb63771ade5f4e7c66bc70f8353
quadritech <96469908+quadritech@users.noreply.github.com>
2025-07-22 00:40:42 +0100

    error fixed in refactoring

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,7 +1,15 @@
+import { useContext } from 'react';
+import UserContext from './UserContext';
+
 const UserProfile = (props) => {
+
+    const userData = useContext(UserContext);
+
     return (
         <div>
+
             <h1>{props.name}</h1>
+            <p>{userData.name}</p>
             <p>Age: {props.age}</p>
             <p>Bio: {props.bio}</p>
         </div>
